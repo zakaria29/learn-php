7dcbe1224cdb17821a4629ef4024a67143a178e5
zakaria29 <zakaria@smktelkom-mlg.sch.id>
2021-07-26 10:47:45 +0700

    Upload Tugas

---
+++ case-study.php
@@ -0,0 +1,17 @@
+<?php
+/** first case */
+$product = [
+    ["name" => "Lenava", "qty" => 4, "price" => 1000000],
+    ["name" => "Aser", "qty" => 2, "price" => 1500000],
+    ["name" => "Toyiba", "qty" => 6, "price" => 2000000],
+    ["name" => "Doll", "qty" => 1, "price" => 1200000],
+    ["name" => "Susa", "qty" => 3, "price" => 1700000],
+];
+/** buat code untuk menghitung total jumlah yg dibeli */
+
+/** second case
+ * buat fungsi untuk menghitung bilangan faktorial
+ * faktorial(5) -> 5 * 4 * 3 * 2 * 1 = 120
+ */
+
+?>
